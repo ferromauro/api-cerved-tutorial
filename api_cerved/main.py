04292b32f2b4db3bc6749fe05a5e4909c473b804
Ferro Mauro <ferromauro@yahoo.it>
2020-11-15 17:40:54 +0100

    MVP

--- api_cerved/main.py
+++ api_cerved/main.py
@@ -1,18 +1,66 @@
 import requests
 import argparse
 import json
-from settings import API_KEY
-
+from settings import CONSUMER_KEY
 from pprint import pprint
-def search_entity(free_text):
-    """Imposta l'url per la ricerca di una entità in base a codice fiscale, partita Iva o ragione sociale."""
-    url = '/v1/entitySearch/live?testoricerca='+free_text
-    return url
-        
+
+def search_entity():
+    """Imposta l'url ed effettua la ricerca di una entità in base a codice fiscale, partita Iva o ragione sociale."""
+    if args.entity:
+        url = '/v1/entitySearch/live?testoricerca='+args.entity
+        try:
+            data = query(url)
+            return data
+        except Exception as e:
+            print(e)
+
+    else:
+        print('Per questo tipo di ricerca è necessario inserire il parametro -e [--entity].')
+        parser.print_help()
+
+def search_profile():
+    """Imposta l'url ed effettua la ricerca di un profilo in base all'id del soggetto."""
+    if args.id_soggetto:
+        url = '/v1/entityProfile/live?id_soggetto='+args.id_soggetto
+        try:
+            data = query(url)
+            return data
+        except Exception as e:
+            print(e)
+    else:
+        print('Per questo tipo di ricerca è necessario inserire il parametro -i [--id_soggetto].')
+        parser.print_help()
+
+def search_contacts():
+    """Imposta l'url ed effettua la ricerca dei contatti di una entità in base all'id del soggetto."""
+    if args.id_soggetto:
+        url = '/v1/entityProfile/contacts?id_soggetto='+args.id_soggetto
+        try:
+            data = query(url)
+            return data
+        except Exception as e:
+            print(e)
+    else:
+        print('Per questo tipo di ricerca è necessario inserire il parametro -i [--id_soggetto].')
+        parser.print_help()
+
+def search_pec():
+    """Imposta l'url ed effettua la ricerca della pec di una entità in base all'id del soggetto."""
+    if args.id_soggetto:
+        url = '/v1/entityProfile/pec?id_soggetto='+args.id_soggetto
+        try:
+            data = query(url)
+            return data
+        except Exception as e:
+            print(e)
+    else:
+        print('Per questo tipo di ricerca è necessario inserire il parametro -i [--id_soggetto].')
+        parser.print_help()
+
 def query(url):
     """Viene impostato l'header con la API Key ed effettua la query in base all'url passato come parametro. Vengono restituiti i dati."""
     headers = { 'accept':'application/json',
-                'apikey': API_KEY 
+                'apikey': CONSUMER_KEY 
               }
     try :
         query_url = 'https://api.cerved.com/cervedApi'+url
@@ -28,26 +76,32 @@ def query(url):
 
 if __name__ == "__main__":
     """API-CERVED-TUTORIAL"""
-    products = ['profile','contacts','pec', 'entity']
+    products = ['entity', 'profile', 'contacts', 'pec']
     parser = argparse.ArgumentParser(description="API Cerved Tutorial")
     parser.add_argument(  '-p', '--product', 
-                        help = 'seleziona un prodotto per la ricerca.',
+                        help = 'Seleziona un prodotto per la ricerca tra quelli compresi nell\'elenco.',
                         required = True,
                         dest = 'product',
                         choices = products
                         )
     parser.add_argument('-e', '--entity',
                         help = 'Testo libero per la ricerca del codice id, accetta: codice fiscale, partita iva, ragione sociale.', 
-                        dest = 'entity')
-    
-    parser.add_argument('-t', '--tax_number',
-                        help = 'inserisci un codice fiscale o una partita IVA.',
-                        dest = 'tax_number'
                         )
-
+    parser.add_argument('-i', '--id_soggetto',
+                        help = 'Testo libero per la ricerca del codice id, accetta: codice fiscale, partita iva, ragione sociale.', 
+                        type = str
+                        )
+    
     args = parser.parse_args()
     if args.product == 'profile':
-        search_profile(args.tax_number)
+        data = search_profile()
     if args.product == 'entity':
-        data = query(search_entity(args.entity))
+        data = search_entity()
+    if args.product == 'contacts':
+        data = search_contacts()
+    if args.product == 'pec':
+        data = search_pec() 
+    if data:
         pprint(data)   
+    else:
+        args.print_help()
